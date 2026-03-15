9ed36bd704f44f02c4acae9fb32f821cdbc0eb84
virdanurlulu <virda.nurlulu@gmail.com>
2026-03-15 20:08:53 +0700

    Delete src directory

--- src/utils/math.js
+++
@@ -1,5 +0,0 @@
-export const clamp = (value, min, max) => Math.min(max, Math.max(min, value));
-export const degToRad = (deg) => (deg * Math.PI) / 180;
-export const radToDeg = (rad) => (rad * 180) / Math.PI;
-export const round = (value, digits = 3) => Number.parseFloat(value.toFixed(digits));
-export const lerp = (a, b, t) => a + (b - a) * t;
