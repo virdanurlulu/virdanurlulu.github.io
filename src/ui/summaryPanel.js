f45e4d58498468d25126a728decf5a2722e1d744
virdanurlulu <virda.nurlulu@gmail.com>
2026-03-16 00:41:10 +0700

    Delete src directory

--- src/ui/summaryPanel.js
+++
@@ -1,8 +0,0 @@
-export function renderSummaryPanel(container, items) {
-  container.innerHTML = items.map((item) => `
-    <div class="summary-item">
-      <span>${item.label}</span>
-      <strong>${item.value}</strong>
-    </div>
-  `).join('');
-}
